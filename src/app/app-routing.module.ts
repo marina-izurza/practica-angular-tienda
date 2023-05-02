d086b562c20f10ed8314eb1bffa415da76537c5f
Marina <77213908+marina-izurza@users.noreply.github.com>
2023-05-02 13:24:38 +0200

    Nosotros y Footer listos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { TiendaComponent } from './tienda/tienda.component';
+import { CategoriaComponent } from './tienda/categorias/categoria/categoria.component';
+import { DetalleComponent } from './tienda/categorias/categoria/detalle/detalle.component';
 
 const routes: Routes = [
   { path: '', component: TiendaComponent },
-  { path: 'nosotros', component: NosotrosComponent }
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'categoria', component:  CategoriaComponent},
+  { path: 'detalle', component:  DetalleComponent}
 ];
 
 @NgModule({
--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./nosotros.component.scss']
 })
 export class NosotrosComponent {
-
+  
 }
